af356820823c70221185c138b04014fe186a1c9e
granen32 <granen32@granen32ui-MacBookPro.local>
2025-03-05 18:43:58 +0900

    feat: 개발공부

--- src/app/(beforelogin)/@modal/(.)i/flow/signup/page.tsx
+++ src/app/(beforelogin)/@modal/(.)i/flow/signup/page.tsx
@@ -3,7 +3,7 @@ import SignupModal from "@/app/(beforeLogin)/_component/SignupModal";
 export default function Signup() {
   return (
     <>
-      난 가로채기지롱 ㅋㅋ
+      {/* 인터셉트 라우터 */}
       <SignupModal/>
     </>
   )
